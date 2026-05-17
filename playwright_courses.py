de99a4b21ff6a5f78f5d895ed721215df0b5d7b4
Ermakova Natalia <natalysolitonsf@yandex.com>
2026-05-17 21:37:26 +0300

    bf task5.6

--- playwright_courses.py
+++ playwright_courses.py
@@ -1,12 +1,15 @@
 from playwright.sync_api import sync_playwright, expect
 
 with sync_playwright() as playwright:
+    # Открываем браузер и создаем новую страницу
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
 
+    # Переходим на страницу входа
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
+    # Заполняем поля регистрации
     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
     email_input.fill('user.name@gmail.com')
 
@@ -28,6 +31,7 @@ with sync_playwright() as playwright:
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
+    # Проверки на наличие текста Courses
     courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
     expect(courses_title).to_be_visible()
     expect(courses_title).to_have_text('Courses')
@@ -35,10 +39,12 @@ with sync_playwright() as playwright:
     empty_view_icon = page.get_by_test_id('courses-list-empty-view-icon')
     expect(empty_view_icon).to_be_visible()
 
+    # Проверки на наличие текста There is no results
     empty_view_title = page.get_by_test_id('courses-list-empty-view-title-text')
     expect(empty_view_title).to_be_visible()
     expect(empty_view_title).to_have_text('There is no results')
 
+    # Проверки на наличие текста Results from the load test pipeline will be displayed here
     empty_view_description = page.get_by_test_id('courses-list-empty-view-description-text')
     expect(empty_view_description).to_be_visible()
     expect(empty_view_description).to_have_text('Results from the load test pipeline will be displayed here')

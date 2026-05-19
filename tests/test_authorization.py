19344bd9abd2cc084751114633302bf94a96cc07
Ermakova Natalia <natalysolitonsf@yandex.com>
2026-05-19 14:49:56 +0300

    6.7 Параметризация Pytest

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -4,20 +4,36 @@ from playwright.sync_api import expect, Page
 
 @pytest.mark.regression
 @pytest.mark.authorization
-# Использование фикстуры 'chromium_page', которая автоматически предоставляет готовую страницу
-def test_wrong_email_or_password_authorization(chromium_page: Page):
-    # Теперь страница передаётся в тест через фикстуру 'chromium_page', браузер не нужно инициализировать вручную
+@pytest.mark.parametrize(
+    "email, password",
+    [
+        ("user.name@gmail.com", "password"),
+        ("user.name@gmail.com", "  "),  # два пробела
+        ("  ", "password"),              # два пробела
+    ],
+    ids=[
+        "Invalid email and password",
+        "Invalid email and empty password",
+        "Empty email and invalid password",
+    ]
+)
+def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
+    # Открываем страницу логина
     chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
+    # Заполняем поле Email параметризованным значением
     email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
-    email_input.fill("user.name@gmail.com")
+    email_input.fill(email)
 
+    # Заполняем поле Password параметризованным значением
     password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
-    password_input.fill("password")
+    password_input.fill(password)
 
+    # Нажимаем кнопку Login
     login_button = chromium_page.get_by_test_id('login-page-login-button')
     login_button.click()
 
+    # Проверяем наличие и текст алерта
     wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
     expect(wrong_email_or_password_alert).to_be_visible()
     expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")

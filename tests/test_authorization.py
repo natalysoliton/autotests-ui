5a478f03a25eeee8be9f9086722f2b2bfd90ca65
Ermakova Natalia <natalysolitonsf@yandex.com>
2026-05-19 15:37:30 +0300

    7.1 Знакомство с PageObject

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,5 +1,6 @@
 import pytest
-from playwright.sync_api import expect, Page
+from playwright.sync_api import Page
+from pages.login_page import LoginPage
 
 
 @pytest.mark.regression
@@ -8,32 +9,12 @@ from playwright.sync_api import expect, Page
     "email, password",
     [
         ("user.name@gmail.com", "password"),
-        ("user.name@gmail.com", "  "),  # два пробела
-        ("  ", "password"),              # два пробела
-    ],
-    ids=[
-        "Invalid email and password",
-        "Invalid email and empty password",
-        "Empty email and invalid password",
+        ("user.name@gmail.com", "  "),
+        ("  ", "password")
     ]
 )
-def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
-    # Открываем страницу логина
-    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
-
-    # Заполняем поле Email параметризованным значением
-    email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
-    email_input.fill(email)
-
-    # Заполняем поле Password параметризованным значением
-    password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
-    password_input.fill(password)
-
-    # Нажимаем кнопку Login
-    login_button = chromium_page.get_by_test_id('login-page-login-button')
-    login_button.click()
-
-    # Проверяем наличие и текст алерта
-    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
-    expect(wrong_email_or_password_alert).to_be_visible()
-    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str):
+    login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+    login_page.fill_login_form(email=email, password=password)
+    login_page.click_login_button()
+    login_page.check_visible_wrong_email_or_password_alert()

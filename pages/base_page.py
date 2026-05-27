527c509eda5bb752fb8971cc30b9311e35dedaa0
Ermakova Natalia <natalysolitonsf@yandex.com>
2026-05-27 17:51:46 +0300

    9.5 Логирование взаимодействий со страницей-homework

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,22 +1,33 @@
 from typing import Pattern
 from playwright.sync_api import Page, expect
 import allure # Импортируем allure
+from tools.logger import get_logger
+import re
+
+logger = get_logger("BASE_PAGE")
 
 class BasePage:
     def __init__(self, page: Page):
         self.page = page
 
     def visit(self, url: str):
-        with allure.step(f'Opening the url "{url}"'):  # Добавили allure.step
-            "Переход на страницу по URL"
-        self.page.goto(url, wait_until='networkidle')
+        step = f'Opening the url "{url}"'
+
+        with allure.step(step):
+            logger.info(step)
+            self.page.goto(url, wait_until="networkidle")
 
     def reload(self):
-        with allure.step(f'Reloading page with url "{self.page.url}"'):  # Добавили allure.step
-            "Перезагрузка страницы"
-        self.page.reload(wait_until='domcontentloaded')
+        step = f'Reloading page with url "{self.page.url}"'
 
-    def check_current_url(self, expected_url: Pattern[str]):
-        with allure.step(f'Checking that current url matches pattern "{expected_url.pattern}"'):  # Добавили allure.step
-            "Проверка текущего URL"
-        expect(self.page).to_have_url(expected_url)
+        with allure.step(step):
+            logger.info(step)
+            self.page.reload(wait_until='domcontentloaded')
+
+    def check_current_url(self, expected_url: re.Pattern[str]):
+        step = f'Checking that current url matches pattern "{expected_url.pattern}"'
+
+        with allure.step(step):
+            logger.info(step)
+            expect(self.page).to_have_url(expected_url)
+
